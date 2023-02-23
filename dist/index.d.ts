6475774f97b468c7da9de8e7bf6cb921379930b7
Justin Kimbrell <jkimbrell@actengage.com>
2023-02-23 11:49:22 -0500

    Removed dist and updated gitignore

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import config from './src/config';
-import FormControl from './src/FormControl.vue';
-import FormControlErrors from './src/FormControlErrors.vue';
-import FormControlLegacy from './src/FormControlLegacy';
-export { config, FormControl, FormControlErrors, FormControlLegacy };
