a4855725356e7b3be97e4800d3ec4b72d957a20a
Justin Kimbrell <jkimbrell@actengage.com>
2023-06-22 12:50:20 -0600

    this is the super commit

--- index.ts
+++ index.ts
@@ -3,9 +3,14 @@ import FormControl from './src/FormControl.vue';
 import FormControlErrors from './src/FormControlErrors.vue';
 import FormControlLegacy from './src/FormControlLegacy';
 
+export type FormControlProps = {
+    name: string,
+    test?: number
+};
+
 export {
-    config,
-    FormControl,
-    FormControlErrors,
-    FormControlLegacy
+config,
+FormControl,
+FormControlErrors,
+FormControlLegacy
 };
